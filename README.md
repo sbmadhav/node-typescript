3625bbdfe6444b9977e273712ea7aac4beb792ee
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2017-01-31 20:28:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TypeScript 2 + Express + Node.js
+
+This is a repository to go with my article on creating an Express web application using TypeScript 2.
+
+## Install
+
+Install the node packages via:
+
+`$ npm install`
+
+And then run the TypeScript Compiler to compile the TypeScript:
+
+`$ npm run compile`
+
+## Starting
+
+To start the server run:
+
+`$ npm start`
---
+++ dist/routes/index.js
@@ -0,0 +1,60 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var route_1 = require("./route");
+/**
+ * / route
+ *
+ * @class User
+ */
+var IndexRoute = (function (_super) {
+    __extends(IndexRoute, _super);
+    /**
+     * Constructor
+     *
+     * @class IndexRoute
+     * @constructor
+     */
+    function IndexRoute() {
+        return _super.call(this) || this;
+    }
+    /**
+     * Create the routes.
+     *
+     * @class IndexRoute
+     * @method create
+     * @static
+     */
+    IndexRoute.create = function (router) {
+        //log
+        console.log("[IndexRoute::create] Creating index route.");
+        //add home page route
+        router.get("/", function (req, res, next) {
+            new IndexRoute().index(req, res, next);
+        });
+    };
+    /**
+     * The home page route.
+     *
+     * @class IndexRoute
+     * @method index
+     * @param req {Request} The express Request object.
+     * @param res {Response} The express Response object.
+     * @next {NextFunction} Execute the next method.
+     */
+    IndexRoute.prototype.index = function (req, res, next) {
+        //set custom title
+        this.title = "TypeScript Exporess Demo";
+        //set message
+        var options = {
+            "message": "Hello from TypeScript and Express"
+        };
+        //render template
+        this.render(req, res, "index", options);
+    };
+    return IndexRoute;
+}(route_1.BaseRoute));
+exports.IndexRoute = IndexRoute;
