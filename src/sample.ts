fcf79d290002ddee505b8a9e518b5fae12afdb4e
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2017-01-31 20:28:38 -0600

    Initial commit

---
+++ src/sample.ts
@@ -0,0 +1,19 @@
+// Simple JS
+let x : string = '10';
+// Data Types
+// Functions
+/*
+* add
+*/
+let add = ( a : number , b : number ) : string => a + b + '';
+// Live documentation
+// Intelisense
+// Control Flow
+let abc : string | number ;
+abc = 10;
+function xyx() {
+
+    if ( typeof abc === 'number' ){
+        return;
+    }
+}
